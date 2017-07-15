def734ac96266fe10f8812412713652b3751a27f
Kaspars Cinis <kaspars.cinis@gmail.com>
2017-07-15 14:17:12 +0200

    Style improvements

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@
             $('#fullpage').fullpage({
                 navigation: true,
                 scrollBar: true,
-                anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+                anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
                 menu: '.navbar-nav'
             });
         }
